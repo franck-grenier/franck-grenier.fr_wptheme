221b813e48c792ac19b8530ab889523ba8cd3650
Franck Grenier <franckg.mail@free.fr>
2014-05-12 22:12:52 +0200

    Init du repo avec la première version de mon thème child de twentytwelve

---
+++ franck-grenier.fr-child/content.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The default template for displaying content. Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+		<div class="featured-post">
+			<?php _e( 'Featured post', 'twentytwelve' ); ?>
+		</div>
+		<?php endif; ?>
+		<header class="entry-header">
+			<?php the_post_thumbnail(); ?>
+			<?php if ( is_single() ) : ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php else : ?>
+			<h1 class="entry-title">
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+			</h1>
+			<?php endif; // is_single() ?>
+			<small class="entry-meta">
+				<?php twentytwelve_entry_meta(); ?>
+				<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+			</small>
+		</header><!-- .entry-header -->
+
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	</article><!-- #post -->
